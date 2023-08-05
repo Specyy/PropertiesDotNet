3f370ca468b5f0ef5166d249a2fe855d2284969e
Specyy <specyydev@gmail.com>
2023-08-05 00:20:13 -0700

    Added read methods to reader and property-writing methods to writers

--- PropertiesDotNet/Core/IPropertiesReader.cs
+++ PropertiesDotNet/Core/IPropertiesReader.cs
@@ -3,7 +3,7 @@
 namespace PropertiesDotNet.Core
 {
     /// <summary>
-    /// Delegate that is called when a <see cref="PropertiesToken"/> is read from a stream.
+    /// Delegate that is called when a token is read from a document.
     /// </summary>
     /// <param name="reader">The reader where the token was read.</param>
     /// <param name="token">The token that was read.</param>
@@ -16,7 +16,7 @@ namespace PropertiesDotNet.Core
     public interface IPropertiesReader : IDisposable
     {
         /// <summary>
-        /// Event raised when a <see cref="PropertiesToken"/> is read from a document.
+        /// Event raised when a token is read from a document.
         /// </summary>
         event TokenRead? TokenRead;
 
@@ -52,5 +52,261 @@ namespace PropertiesDotNet.Core
         /// <exception cref="PropertiesException">If an error was encountered whilst trying to
         /// read the document, and exceptions are configured to be thrown.</exception>
         bool MoveNext();
+    }
+
+    /// <summary>
+    /// Provides extension methods for an <see cref="IPropertiesReader"/>.
+    /// </summary>
+    public static class PropertiesReaderExtensions
+    {
+        /// <summary>
+        /// Reads the current token, or the next token if the reader is at the start state.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <returns>The token that was read</returns>
+        /// <exception cref="PropertiesException">If an error was encountered whilst trying to
+        /// read the document, and exceptions are configured to be thrown.</exception>
+        public static PropertiesToken Read(this IPropertiesReader reader)
+        {
+            var token = reader.Token;
+            reader.MoveNext();
+            return token.Type == PropertiesTokenType.None ? reader.Token : token;
+        }
+
+        /// <summary>
+        /// Moves to the next property within the document.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <returns>Whether or not the next property was moved to.</returns>
+        public static bool MoveToContent(this IPropertiesReader reader)
+        {
+            do
+            {
+                switch (reader.Token.Type)
+                {
+                    case PropertiesTokenType.Key:
+                    case PropertiesTokenType.Assigner:
+                    case PropertiesTokenType.Value:
+                        return true;
+                    case PropertiesTokenType.Error:
+                        return false;
+                    default:
+                        break;
+                }
+            } while (reader.MoveNext());
+
+            return false;
+        }
+
+        /// <summary>
+        /// Reads the current property, or the next property if the reader is not at a property.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <param name="key">The property key.</param>
+        /// <param name="value">The property value.</param>
+        /// <returns>Whether the property could be read.</returns>
+        /// <exception cref="PropertiesException">If an error was encountered whilst trying to
+        /// read the document, and exceptions are configured to be thrown.</exception>
+        public static bool TryReadProperty(this IPropertiesReader reader, out string key, out string? value)
+        {
+            var token = reader.Token;
+
+            if (token.Type == PropertiesTokenType.None)
+            {
+                reader.MoveNext();
+                token = reader.Token;
+            }
+
+            if (token.Type == PropertiesTokenType.Key)
+            {
+                key = token.Text;
+
+                if (reader.MoveNext())
+                {
+                    token = reader.Token;
+
+                    if (token.Type == PropertiesTokenType.Assigner)
+                    {
+                        reader.MoveNext();
+                        token = reader.Token;
+                    }
+
+                    if (token.Type == PropertiesTokenType.Value)
+                    {
+                        value = token.Text;
+                        return true;
+                    }
+                }
+                else
+                {
+                    value = null;
+                    return true;
+                }
+            }
+
+            key = null!;
+            value = null;
+            return false;
+        }
+
+        /// <summary>
+        /// Reads the text value for the current token as a <see langword="byte"/>.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <returns>The token value as a <see langword="byte"/>.</returns>
+        /// <exception cref="PropertiesException">If an error was encountered whilst trying to
+        /// read the document, and exceptions are configured to be thrown.</exception>
+        public static byte ReadByte(this IPropertiesReader reader)
+        {
+            return byte.Parse(reader.Read().Text);
+        }
+
+        /// <summary>
+        /// Reads the text value for the current token as a <see langword="sbyte"/>.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <returns>The token value as a <see langword="sbyte"/>.</returns>
+        /// <exception cref="PropertiesException">If an error was encountered whilst trying to
+        /// read the document, and exceptions are configured to be thrown.</exception>
+        public static sbyte ReadSByte(this IPropertiesReader reader)
+        {
+            return sbyte.Parse(reader.Read().Text);
+        }
+
+        /// <summary>
+        /// Reads the text value for the current token as a <see langword="ushort"/>.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <returns>The token value as a <see langword="ushort"/>.</returns>
+        /// <exception cref="PropertiesException">If an error was encountered whilst trying to
+        /// read the document, and exceptions are configured to be thrown.</exception>
+        public static ushort ReadUInt16(this IPropertiesReader reader)
+        {
+            return ushort.Parse(reader.Read().Text);
+        }
+
+        /// <summary>
+        /// Reads the text value for the current token as a <see langword="short"/>.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <returns>The token value as a <see langword="short"/>.</returns>
+        /// <exception cref="PropertiesException">If an error was encountered whilst trying to
+        /// read the document, and exceptions are configured to be thrown.</exception>
+        public static short ReadInt16(this IPropertiesReader reader)
+        {
+            return short.Parse(reader.Read().Text);
+        }
+
+        /// <summary>
+        /// Reads the text value for the current token as a <see langword="uint"/>.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <returns>The token value as a <see langword="uint"/>.</returns>
+        /// <exception cref="PropertiesException">If an error was encountered whilst trying to
+        /// read the document, and exceptions are configured to be thrown.</exception>
+        public static uint ReadUInt32(this IPropertiesReader reader)
+        {
+            return uint.Parse(reader.Read().Text);
+        }
+
+        /// <summary>
+        /// Reads the text value for the current token as a <see langword="int"/>.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <returns>The token value as a <see langword="int"/>.</returns>
+        /// <exception cref="PropertiesException">If an error was encountered whilst trying to
+        /// read the document, and exceptions are configured to be thrown.</exception>
+        public static int ReadInt32(this IPropertiesReader reader)
+        {
+            return int.Parse(reader.Read().Text);
+        }
+
+        /// <summary>
+        /// Reads the text value for the current token as a <see langword="ulong"/>.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <returns>The token value as a <see langword="ulong"/>.</returns>
+        /// <exception cref="PropertiesException">If an error was encountered whilst trying to
+        /// read the document, and exceptions are configured to be thrown.</exception>
+        public static ulong ReadUInt64(this IPropertiesReader reader)
+        {
+            return ulong.Parse(reader.Read().Text);
+        }
+
+        /// <summary>
+        /// Reads the text value for the current token as a <see langword="long"/>.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <returns>The token value as a <see langword="long"/>.</returns>
+        /// <exception cref="PropertiesException">If an error was encountered whilst trying to
+        /// read the document, and exceptions are configured to be thrown.</exception>
+        public static long ReadInt64(this IPropertiesReader reader)
+        {
+            return long.Parse(reader.Read().Text);
+        }
+
+        /// <summary>
+        /// Reads the text value for the current token as a <see langword="float"/>.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <returns>The token value as a <see langword="float"/>.</returns>
+        /// <exception cref="PropertiesException">If an error was encountered whilst trying to
+        /// read the document, and exceptions are configured to be thrown.</exception>
+        public static float ReadFloat(this IPropertiesReader reader)
+        {
+            return float.Parse(reader.Read().Text);
+        }
+
+        /// <summary>
+        /// Reads the text value for the current token as a <see langword="double"/>.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <returns>The token value as a <see langword="double"/>.</returns>
+        /// <exception cref="PropertiesException">If an error was encountered whilst trying to
+        /// read the document, and exceptions are configured to be thrown.</exception>
+        public static double ReadDouble(this IPropertiesReader reader)
+        {
+            return double.Parse(reader.Read().Text);
+        }
+
+        /// <summary>
+        /// Reads the text value for the current token as a <see langword="decimal"/>.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <returns>The token value as a <see langword="decimal"/>.</returns>
+        /// <exception cref="PropertiesException">If an error was encountered whilst trying to
+        /// read the document, and exceptions are configured to be thrown.</exception>
+        public static decimal ReadDecimal(this IPropertiesReader reader)
+        {
+            return decimal.Parse(reader.Read().Text);
+        }
+
+        /// <summary>
+        /// Reads the text value for the current token as a <see langword="bool"/>.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <returns>The token value as a <see langword="bool"/>.</returns>
+        /// <exception cref="PropertiesException">If an error was encountered whilst trying to
+        /// read the document, and exceptions are configured to be thrown.</exception>
+        public static bool ReadBool(this IPropertiesReader reader)
+        {
+            string text = reader.Read().Text;
+            if (bool.TryParse(text, out bool value))
+                return value;
+
+            return !text.Equals('0') && (text.Equals('1') ? true : throw new ArgumentException(nameof(value)));
+        }
+
+        /// <summary>
+        /// Reads the text value for the current token as a <see langword="char"/>.
+        /// </summary>
+        /// <param name="reader">The underlying reader.</param>
+        /// <returns>The token value as a <see langword="char"/>.</returns>
+        /// <exception cref="PropertiesException">If an error was encountered whilst trying to
+        /// read the document, and exceptions are configured to be thrown.</exception>
+        public static char ReadChar(this IPropertiesReader reader)
+        {
+            return char.Parse(reader.Read().Text);
+        }
     }
 }
--- PropertiesDotNet/Core/IPropertiesWriter.cs
+++ PropertiesDotNet/Core/IPropertiesWriter.cs
@@ -3,7 +3,7 @@ using System;
 namespace PropertiesDotNet.Core
 {
     /// <summary>
-    /// Delegate called when a <see cref="PropertiesToken"/> is written.
+    /// Delegate called when a token is written.
     /// </summary>
     /// <param name="writer">The writer where the token was written.</param>
     /// <param name="token">The token that was written.</param>
@@ -11,12 +11,12 @@ namespace PropertiesDotNet.Core
 
     /// <summary>
     /// Represents a ".properties" document writer used to write document information, in the form of
-    /// <see cref="PropertiesToken"/>s. This interface allows for multiple custom implementations of ".properties" writers.
+    /// tokens. This interface allows for multiple custom implementations of ".properties" writers.
     /// </summary> 
     public interface IPropertiesWriter : IDisposable
     {
         /// <summary>
-        /// Event raised when a <see cref="PropertiesToken"/> is written into an <see cref="IPropertiesWriter"/>.
+        /// Event raised when a token is written into an <see cref="IPropertiesWriter"/>.
         /// </summary>
         event TokenWritten? TokenWritten;
 
@@ -29,8 +29,8 @@ namespace PropertiesDotNet.Core
         /// Writes or emits the token to this writer.
         /// </summary>
         /// <param name="token">The token to write.</param>
-        /// <exception cref="PropertiesException">If an incorrect <paramref name="token"/> was passed
-        /// as an argument, depending on the context.</exception>
+        /// <exception cref="PropertiesException">If an incorrect token was passed as an argument, 
+        /// depending on the context.</exception>
         /// <returns>true if the token was successfully written; false otherwise.</returns>
         bool Write(PropertiesToken token);
 
@@ -38,5 +38,23 @@ namespace PropertiesDotNet.Core
         /// Dumps all the cached tokens to the underlying stream.
         /// </summary>
         void Flush();
+    }
+
+    /// <summary>
+    /// Provides extensions methods for an <see cref="IPropertiesWriter"/>.
+    /// </summary>
+    public static class PropertiesWriterExtensions
+    {
+        /// <summary>
+        /// Writes a new property to the document.
+        /// </summary>
+        /// <param name="writer">The underlying writer</param>
+        /// <param name="key">The key for the property</param>
+        /// <param name="value">The value of the property.</param>
+        /// <returns>true if the property could be written; false otherwise.</returns>
+        public static bool WriteProperty(this IPropertiesWriter writer, string key, string? value)
+        {
+            return writer.Write(new PropertiesToken(PropertiesTokenType.Key, key)) && writer.Write(new PropertiesToken(PropertiesTokenType.Value, value));
+        }
     }
 }
