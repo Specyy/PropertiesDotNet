3596e3b191d9c189957397e68d3091f6517f8f7a
Specyy <specyydev@gmail.com>
2023-08-15 21:53:17 -0700

    Re-made test project.

---
+++ PropertiesDotNet.Test/PropertiesReaderTests.cs
@@ -0,0 +1,16 @@
+namespace PropertiesDotNet.Test
+{
+    public class PropertiesReaderTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
